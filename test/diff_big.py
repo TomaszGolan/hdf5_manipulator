28bbc7a10c08b70c37c87d136f9e99a33d586436
Tomasz Golan <tomasz.golan@gmail.com>
2016-04-27 16:01:13 -0500

    partcheck for diff_big

--- test/diff_big.py
+++ test/diff_big.py
@@ -11,9 +11,59 @@ import imp
 msg = imp.load_source('msg', '../msg.py')
 check = imp.load_source('check', '../check.py')
 
+
+def fullcheck(data1, data2, key):
+
+    """Do full comparison entry by entry.
+
+    Keyword arguments:
+    data1 -- numpy array with data
+    data2 -- numpy array with data
+    key -- dataset being checked
+    """
+
+    msg.warning("%s dataset too big to fit in memory. "
+                "Comparing entry by entry." % key)
+    for i, val in enumerate(data1):
+        sys.stdout.write("Comparing %(key)s: %(progress).2f%%       \r"
+                         % {"key": key,
+                            "progress": 100.0 * i / len(data1)})
+        sys.stdout.flush()
+        if not np.array_equal(data1[i], data2[i]):
+            msg.error("Different entry (%(id) in %(key)s dataset."
+                      % {"key": key, "id": i})
+            sys.exit(1)
+
+
+def partcheck(data1, data2, key, n=100):
+
+    """Do full comparison entry by entry.
+
+    Keyword arguments:
+    data1 -- numpy array with data
+    data2 -- numpy array with data
+    key -- dataset being checked
+    n -- number of entries to compare in each part of the file
+    """
+
+    msg.warning("%(key)s dataset too big to fit in memory. "
+                "Comparing first / last / radnom %(n)d entries."
+                % {"key": key, "n": n})
+
+    N = len(data1)
+    entries = range(n)
+    entries.extend(range(N-n, N))
+    entries.extend(np.random.randint(low=n, high=N-n, size=n))
+
+    for i in entries:
+        if not np.array_equal(data1[i], data2[i]):
+            msg.error("Different entry (%(id) in %(key)s dataset."
+                      % {"key": key, "id": i})
+            sys.exit(1)
+
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print "usage: ./diff file1 file2"
+    if len(sys.argv) < 3:
+        print "usage: ./diff file1 file2 [fullcheck]"
         sys.exit(1)
 
     in1 = h5py.File(sys.argv[1], 'r')
@@ -38,17 +88,11 @@ if __name__ == '__main__':
             else:
                 msg.info("%s match." % key)
         except:
-            msg.warning("%s dataset too big to fit in memory. "
-                        "Comparing entry by entry." % key)
-            for i, val in enumerate(in1[key]):
-                sys.stdout.write("Comparing %(key)s: %(progress).2f%%       \r"
-                                 % {"key": key,
-                                    "progress": 100.0 * i / len(in1[key])})
-                sys.stdout.flush()
-                if not np.array_equal(in1[key][i], in2[key][i]):
-                    msg.error("Different entry (%(id) in %(key)s dataset."
-                              % {"key": key, "id": i})
-                    sys.exit(1)
+            if len(sys.argv) == 4 and sys.argv[3] == "fullcheck":
+                fullcheck(in1[key], in2[key], key)
+            else:
+                partcheck(in1[key], in2[key], key)
+
             msg.info("%s match." % key)
 
     msg.info("Files are the same.")
