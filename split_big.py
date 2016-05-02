0896563cefdd5676d31684f7f9ebc312574cf353
Tomasz Golan <tomasz.golan@gmail.com>
2016-05-02 12:02:15 -0500

    merge big

--- split_big.py
+++ split_big.py
@@ -11,51 +11,8 @@ import hdf5
 import h5py
 import check
 from combine_big import load
-
-
-def generate_filelist(prefix, old_size, new_size):
-
-    """Generate filenames for output files
-    and return as a dict (file: [begin, end]).
-
-    Keyword arguments:
-    prefix -- common path/to/basename
-    old_size -- size of input hdf5 files
-    new_size -- requested size for output hdf5 files
-    """
-
-    if new_size >= old_size:
-        msg.error("Use splitter wisely...")
-        sys.exit(1)
-
-    nof_files, leftover = old_size / new_size, old_size % new_size
-
-    files = OrderedDict()
-
-    for i in range(nof_files + int(leftover > 0)):
-        filename = "%(prefix)s_%(id)03d.hdf5" % {"prefix": prefix, "id": i}
-        begin = i * new_size
-        end = (i + 1) * new_size if i < nof_files else i * new_size + leftover
-        files[filename] = [begin, end]
-
-    return files
-
-
-def save_filelist(filename, filelist):
-
-    """Save the list of created files.
-
-    Keyword arguments:
-    filename -- the path to txt file
-    filelist -- the list of files
-    """
-
-    f = open(filename, 'w')
-
-    for fn in filelist:
-        print >>f, os.path.abspath(fn)
-
-    f.close()
+from split import generate_filelist
+from split import save_filelist
 
 if __name__ == '__main__':
 
