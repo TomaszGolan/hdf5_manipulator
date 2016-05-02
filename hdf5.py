96cb476e25eae3900f97ff248724e1d228589f0d
Tomasz Golan <tomasz.golan@gmail.com>
2016-05-02 10:24:25 -0500

    split big files

--- hdf5.py
+++ hdf5.py
@@ -37,8 +37,8 @@ def save(filename, data):
     f = h5py.File(filename, 'w')
 
     for key in data:
-        f.create_dataset(key, data[key].shape, dtype=data[key].dtype)[...] \
-            = data[key]
+        f.create_dataset(key, data[key].shape, dtype=data[key].dtype,
+                         compression='gzip')[...] = data[key]
 
     f.close()
 
@@ -60,3 +60,25 @@ def save_subset(filename, data, begin, end):
         subset[key] = data[key][begin:end]
 
     save(filename, subset)
+
+
+def save_subset_big(filename, data, begin, end):
+
+    """Create hdf5 file with subset [begin, end) of given data.
+
+    Keyword arguments:
+    filename -- the full path to hdf5 file
+    data -- input file
+    begin -- start saving from index=i
+    end -- finish savin at index=end
+    """
+
+    o = h5py.File(filename, 'w')
+
+    for key in data:
+        shape = list(data[key].shape)
+        shape[0] = end - begin
+        o.create_dataset(key, shape, dtype=data[key].dtype,
+                         compression='gzip')[...] = data[key][begin:end]
+
+    o.close()
---
+++ split_big.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+"""
+Split hdf5 (big) file
+"""
+import os
+import sys
+from collections import OrderedDict
+from parser import get_args_split as parser
+import msg
+import hdf5
+import h5py
+import check
+from combine_big import load
+
+
+def generate_filelist(prefix, old_size, new_size):
+
+    """Generate filenames for output files
+    and return as a dict (file: [begin, end]).
+
+    Keyword arguments:
+    prefix -- common path/to/basename
+    old_size -- size of input hdf5 files
+    new_size -- requested size for output hdf5 files
+    """
+
+    if new_size >= old_size:
+        msg.error("Use splitter wisely...")
+        sys.exit(1)
+
+    nof_files, leftover = old_size / new_size, old_size % new_size
+
+    files = OrderedDict()
+
+    for i in range(nof_files + int(leftover > 0)):
+        filename = "%(prefix)s_%(id)03d.hdf5" % {"prefix": prefix, "id": i}
+        begin = i * new_size
+        end = (i + 1) * new_size if i < nof_files else i * new_size + leftover
+        files[filename] = [begin, end]
+
+    return files
+
+
+def save_filelist(filename, filelist):
+
+    """Save the list of created files.
+
+    Keyword arguments:
+    filename -- the path to txt file
+    filelist -- the list of files
+    """
+
+    f = open(filename, 'w')
+
+    for fn in filelist:
+        print >>f, os.path.abspath(fn)
+
+    f.close()
+
+if __name__ == '__main__':
+
+    msg.box("HDF5 MANIPULATOR: SPLIT")
+
+    args = parser()
+
+    data = load(args.input)
+
+    filelist = generate_filelist(
+        args.prefix or os.path.splitext(args.input)[0],
+        check.get_size(data), int(args.size))
+
+    print "\nSaving output files:\n"
+
+    for f, r in filelist.iteritems():
+        msg.list_fileinfo(f, r)
+        hdf5.save_subset_big(f, data, r[0], r[1])
+
+    if args.filelist:
+        save_filelist(args.filelist, filelist.keys())
+
+    data.close()
+
+    msg.info("Done")
