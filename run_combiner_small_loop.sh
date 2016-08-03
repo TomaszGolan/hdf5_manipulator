5e1f8d6b44dd9e4d56df5ffa2e1620f0cb5608ce
Gabriel Perdue <perdue@fnal.gov>
2016-08-03 16:41:50 -0500

    yadda, yadda

--- run_combiner_small_loop.sh
+++ run_combiner_small_loop.sh
@@ -10,15 +10,27 @@ elif [[ $# > 1 ]]; then
   STOP=$2
 fi
 
+SAMPLE=minerva1mc
 
 for i in `seq ${START} 1 ${STOP}`
 do
   filenum=`echo $i | perl -ne 'printf "%04d",$_;'`
 
-  INP1="minosmatch_nukecczdefs_tracker_127x72x36_me1Amc_${filenum}.hdf5"
-  INP2="minosmatch_hadmult_me1Amc_${filenum}.hdf5"
-  OUTP="minosmatch_hadmult_tracker_127x72x36_me1Amc_${filenum}.hdf5"
+  INP1="minosmatch_nukecczdefs_tracker_127x72x36_${SAMPLE}_${filenum}.hdf5"
+  INP2="minosmatch_hadmult_${SAMPLE}_${filenum}.hdf5"
+  OUTP="minosmatch_hadmult_tracker_127x72x36_${SAMPLE}_${filenum}.hdf5"
 
+cat << EOF
+  time python combine.py \
+    --input1 $INP1 \
+    --input2 $INP2 \
+    --output $OUTP \
+    --match eventids \
+    --keys2 n-hadmultmeas,n-kaons,n-neutrons,n-others,n-pi0s,n-pions,n-protons \
+    --do-not-warn --show-progress
+
+  python fuelme.py $OUTP 0.83 0.10
+EOF
   time python combine.py \
     --input1 $INP1 \
     --input2 $INP2 \
@@ -29,10 +41,21 @@ do
 
   python fuelme.py $OUTP 0.83 0.10
 
-  INP1="minosmatch_nukecczdefs_tracker_127x72x36_me1Amc_${filenum}.hdf5"
-  INP2="minosmatch_singlepi0_me1Amc_${filenum}.hdf5"
-  OUTP="minosmatch_singlepi0_tracker_127x72x36_me1Amc_${filenum}.hdf5"
+  INP1="minosmatch_nukecczdefs_tracker_127x72x36_${SAMPLE}_${filenum}.hdf5"
+  INP2="minosmatch_singlepi0_${SAMPLE}_${filenum}.hdf5"
+  OUTP="minosmatch_singlepi0_tracker_127x72x36_${SAMPLE}_${filenum}.hdf5"
 
+cat << EOF
+  time python combine.py \
+    --input1 $INP1 \
+    --input2 $INP2 \
+    --output $OUTP \
+    --match eventids \
+    --keys2 i-signal,n-hadmultmeas,n-kaons,n-neutrons,n-others,n-pi0s,n-pions,n-protons \
+    --do-not-warn --show-progress
+
+  python fuelme.py $OUTP 0.83 0.10
+EOF
   time python combine.py \
     --input1 $INP1 \
     --input2 $INP2 \
