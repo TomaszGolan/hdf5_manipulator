8f362f3275f14eb124fa7f9ff190ee5ca5b0ecba
Gabriel Perdue <perdue@fnal.gov>
2016-08-03 16:41:23 -0500

    blah, blah

--- run_combiner.sh
+++ run_combiner.sh
@@ -6,15 +6,15 @@
 # OUTP="minosmatch_hadmult_127x94x47_xuv_me1Bmc.hdf5"
 
 # keys for these files should include i-signal instead of n-hadmultmeas
-INP1="minosmatch_singlepi0_me1Bmc.hdf5"
-INP2="minosmatch_nukecczdefs_127x50x25_xuv_me1Bmc.hdf5"
-OUTP="minosmatch_singlepi0_127x50x25_xuv_me1Bmc.hdf5"
+INP1="minosmatch_muondat_me1Bmc.hdf5"
+INP2="../HDF5files/minosmatch_nukecczdefs_genallz_pcodecap66_127x50x25_xuv_me1Bmc.hdf5"
+OUTP="minosmatch_nukecczdefs_genallz_pcodecap66_muondat_127x50x25_xuv_me1Bmc.hdf5"
 
 python combine_big.py \
   --input1 $INP1 \
   --input2 $INP2 \
   --output $OUTP \
   --match eventids \
-  --keys1 i-signal,n-kaons,n-neutrons,n-others,n-pi0s,n-pions,n-protons
+  --keys1 muon_data
 
 python fuelme.py $OUTP 0.83 0.10
