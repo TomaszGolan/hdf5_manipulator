949a26e494657e1964476131b8f0b39b21f6b55d
Gabriel Perdue <perdue@fnal.gov>
2016-09-13 15:42:25 -0500

    check to see if any files in a long list (similar but for id number) are missing

---
+++ exist_checker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in {0..113}
+do
+  filenum=`echo $i | perl -ne 'printf "%04d",$_;'`
+  # ls minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_txtutv_vtxinfo_me1Bmc_${filenum}.hdf5 | grep "No"
+  # ls minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_txtutv_me1Bmc_${filenum}.hdf5 | grep "No"
+  ls minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_minerva13Cmc_${filenum}.hdf5 | grep "No"
+done
