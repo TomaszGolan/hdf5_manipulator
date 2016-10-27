8efc21222b169364fd6e42c97465083e793ae7b3
Gabriel Perdue <perdue@fnal.gov>
2016-10-27 08:38:55 -0500

    keep up with me1A mc processing

--- run_merge.sh
+++ run_merge.sh
@@ -1,40 +1,10 @@
 #!/bin/bash
 
 PYEXEC="merge_big.py"
+SAMPLE=me1Bmc
 
-SAMPLE=me1Adata
-
-# INP="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_txtutv_vtxinfo_${SAMPLE}_"
-# OUT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_txtutv_vtxinfo_${SAMPLE}.hdf5"
-# cat << EOF
-# python $PYEXEC --input $INP --output $OUT
-# python fuelme.py $OUT 0.83 0.10
-# EOF
-# python $PYEXEC --input $INP --output $OUT
-# python fuelme.py $OUT 0.83 0.10
-
-# INP="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_txtutv_${SAMPLE}_"
-# OUT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_txtutv_${SAMPLE}.hdf5"
-# cat << EOF
-# python $PYEXEC --input $INP --output $OUT
-# python fuelme.py $OUT 0.83 0.10
-# EOF
-# python $PYEXEC --input $INP --output $OUT
-# python fuelme.py $OUT 0.83 0.10
-
-# INP="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_txtutv_muondat_${SAMPLE}_"
-# OUT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_txtutv_muondat_${SAMPLE}.hdf5"
-# cat << EOF
-# python $PYEXEC --input $INP --output $OUT
-# python fuelme.py $OUT 0.83 0.10
-# EOF
-# python $PYEXEC --input $INP --output $OUT
-# python fuelme.py $OUT 0.83 0.10
-
-SAMPLE=minerva13Bmc
-
-INP="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_${SAMPLE}_"
-OUT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_${SAMPLE}.hdf5"
+INP="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Bmc_pt1_"
+OUT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_me1Bmc.hdf5"
 cat << EOF
 python $PYEXEC --input $INP --output $OUT
 python fuelme.py $OUT 0.83 0.10
