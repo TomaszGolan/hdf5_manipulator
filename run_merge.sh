5426afa573380ea1716d8e2d3aae023fe4e1fb23
Gabriel Perdue <perdue@fnal.gov>
2017-03-14 11:18:55 -0500

    merge the kine files for me1Bmc

--- run_merge.sh
+++ run_merge.sh
@@ -2,23 +2,14 @@
 
 PYEXEC="merge_big.py"
 
-SAMPLE=minerva1nofsimc
+# SAMPLE=minerva1nofsimc
 
 # note
 #  python fuelme.py $OUT 0.83 0.10
 # was the "historic standard", but switching as of me1Amc energy+time lattice
 
-INP="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_${SAMPLE}_"
-OUT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xtxutuvtv_${SAMPLE}.hdf5"
-cat << EOF
-python $PYEXEC --input $INP --output $OUT
-python fuelme.py $OUT 0.86 0.07
-EOF
-python $PYEXEC --input $INP --output $OUT
-python fuelme.py $OUT 0.86 0.07
-
-INP="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_${SAMPLE}_"
-OUT="minosmatch_nukecczdefs_genallzwitht_pcodecap66_127x50x25_xuv_${SAMPLE}.hdf5"
+INP="minosmatch_kinedat_me1Bmc_"
+OUT="minosmatch_kinedat_me1Bmc.hdf5"
 cat << EOF
 python $PYEXEC --input $INP --output $OUT
 python fuelme.py $OUT 0.86 0.07
