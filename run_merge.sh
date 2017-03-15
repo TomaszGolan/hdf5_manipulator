a64ea2fa0eac6fc7f605c9e26d578e79df4eb348
Gabriel Perdue <perdue@fnal.gov>
2017-03-15 13:48:49 -0500

    merge kine+image shards

--- run_merge.sh
+++ run_merge.sh
@@ -2,14 +2,12 @@
 
 PYEXEC="merge_big.py"
 
-# SAMPLE=minerva1nofsimc
-
 # note
 #  python fuelme.py $OUT 0.83 0.10
 # was the "historic standard", but switching as of me1Amc energy+time lattice
 
-INP="minosmatch_kinedat_me1Bmc_"
-OUT="minosmatch_kinedat_me1Bmc.hdf5"
+INP="minosmatch_nukecczdefs_genallz_pcodecap66_kinedat_127x50x25_xuv_me1Bmc_"
+OUT="minosmatch_nukecczdefs_genallz_pcodecap66_kinedat_127x50x25_xuv_me1Bmc.hdf5"
 cat << EOF
 python $PYEXEC --input $INP --output $OUT
 python fuelme.py $OUT 0.86 0.07
