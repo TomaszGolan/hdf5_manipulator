4d2cad6a8195a0af4163ca404a897e8947197646
Gabriel Perdue <perdue@fnal.gov>
2017-03-20 12:39:08 -0500

    clean up comments a bit

--- run_combiner.sh
+++ run_combiner.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# keys should include n-hadmultmeas instead of i-signal
-# INP1="minosmatch_hadmult_me1Bmc.hdf5"
-# INP2="minosmatch_nukecczdefs_127x94x47_me1Bmc.hdf5"
-# OUTP="minosmatch_hadmult_127x94x47_xuv_me1Bmc.hdf5"
-
-# keys for these files should include i-signal instead of n-hadmultmeas
 INP1_BASE="../HDF5files/minosmatch_nukecczdefs_genallz_pcodecap66_127x50x25_xuv_me1Bmc_100kevts"
 INP2="minosmatch_kinedat_me1Bmc.hdf5"
 OUTP_BASE="minosmatch_nukecczdefs_genallz_pcodecap66_kinedat_127x50x25_xuv_me1Bmc"
 
-# START=0
-START=1
-# STOP=0
+START=0
 STOP=44
 
 for i in `seq ${START} 1 ${STOP}`
