34d19a26018ab258e807db6a77b2ff69a64f1a69
Gabriel Perdue <gnperdue@me.com>
2017-08-17 06:31:36 -0500

    start work on uneven splitter

--- split.py
+++ split.py
@@ -39,6 +39,34 @@ def generate_filelist(prefix, old_size, new_size):
     return files
 
 
+def generate_uneven_filelist(prefix, old_size, new_sizelist):
+
+    """Generate filenames for output files
+    and return as a dict (file: [begin, end]).
+
+    Keyword arguments:
+    prefix -- common path/to/basename
+    old_size -- size of input hdf5 files
+    new_sizelist -- list of tuples for the different files (name, (start, stop))
+    """
+
+    if new_size >= old_size:
+        msg.error("Use splitter wisely...")
+        sys.exit(1)
+
+    nof_files, leftover = old_size / new_size, old_size % new_size
+
+    files = OrderedDict()
+
+    for i in range(nof_files + int(leftover > 0)):
+        filename = "%(prefix)s_%(id)03d.hdf5" % {"prefix": prefix, "id": i}
+        begin = i * new_size
+        end = (i + 1) * new_size if i < nof_files else i * new_size + leftover
+        files[filename] = [begin, end]
+
+    return files
+
+
 def save_filelist(filename, filelist):
 
     """Save the list of created files.
